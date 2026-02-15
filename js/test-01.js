c78c5ced80ca7ce6a1a1e55b1e6393f609aad342
Yurii Volkov <sir.volkov@outlook.com>
2026-02-15 15:35:07 +0200

    upd test- 01.

---
+++ js/test-01.js
@@ -0,0 +1,33 @@
+// let number;
+// const value = prompt("Enter a number:", number)
+
+// const message = 15 > value ? `15 is greater than ${value}` : `15 is not greater than ${value}`;
+
+// console.log(message);
+
+//===============================
+
+// let message;
+// let number;
+// const value = prompt("Enter a number:", number);
+
+// switch (true) {
+//     case value > 15:
+//         message = `15 is less than ${value}`;
+//         break;
+//     case value < 15:
+//         message = `15 is greater than ${value}`;
+//         break;
+//     default:
+//         message = `15 is equal to ${value}`;
+// }
+
+// console.log(message);
+
+//===============================
+           //0123456789012
+const str = "Hello, World!";
+
+for (let i = 0; i < str.length; i += 1) { 
+    console.log(str[i]);
+}

9ce1148849d610d5701745fbbda76e44518289e2
Yurii Volkov <sir.volkov@outlook.com>
2026-02-19 06:15:34 +0200

    upd objects.js

--- js/homework/objects.js
+++ js/homework/objects.js
@@ -1,22 +1,22 @@
 console.log("-------------- example -------------------");
 
 const books = {
-    books: ['The Last Kingdom'],
-    author: 'Bernard Cornwell',
-    genres: ['hidtorical prose', 'adventure'],
-    isPublic: true,
-    rating: 8.38,
-    getBooks(){
-        return this.books
-    },
-    addBooks(bookName){
-        this.books.push(bookName)
-    },
-    removeBook(bookName) {
-        const bookIndex = this.books.indexOf(bookName);
-        this.books.splice(bookIndex, 1)
-    }
-}
+  books: ["The Last Kingdom"],
+  author: "Bernard Cornwell",
+  genres: ["hidtorical prose", "adventure"],
+  isPublic: true,
+  rating: 8.38,
+  getBooks() {
+    return this.books;
+  },
+  addBooks(bookName) {
+    this.books.push(bookName);
+  },
+  removeBook(bookName) {
+    const bookIndex = this.books.indexOf(bookName);
+    this.books.splice(bookIndex, 1);
+  },
+};
 
 console.log(books);
 
@@ -28,15 +28,15 @@ console.log(books.getBooks());
 console.log("-------------- example -------------------");
 
 for (const key in books) {
-    if (!Object.hasOwn(books, key)) continue;
-    
-    const element = books[key];
-    console.log(element);
+  if (!Object.hasOwn(books, key)) continue;
+
+  const element = books[key];
+  console.log(element);
 }
 
 const animal = {
-    legs: 4
-}
+  legs: 4,
+};
 
 console.log("-------------- example -------------------");
 
@@ -58,7 +58,7 @@ console.log("-------------- example -------------------");
 const keys = Object.keys(books);
 
 for (const item of keys) {
-    console.log(item);
+  console.log(item);
 }
 
 console.log("-------------- example -------------------");
@@ -66,7 +66,7 @@ console.log("-------------- example -------------------");
 const values = Object.values(books);
 
 for (const item of values) {
-    console.log(item);
+  console.log(item);
 }
 
 console.log("-------------- example -------------------");
@@ -76,7 +76,7 @@ const entries = Object.entries(books);
 console.log(entries);
 
 for (const item of entries) {
-    console.log(item);
+  console.log(item);
 }
 
 console.log("-------------- example -------------------");
@@ -103,18 +103,18 @@ const bookNames = [];
 let totalRating = 0;
 
 for (const item of booksCollection) {
-    console.log(item.title);
-    console.log(item.author);
-    console.log(item.rating);
+  console.log(item.title);
+  console.log(item.author);
+  console.log(item.rating);
 
-    bookNames.push(item.author);
-    totalRating += item.rating;
+  bookNames.push(item.author);
+  totalRating += item.rating;
 }
 
 console.log(bookNames);
 console.log(totalRating);
 
-const averageRating = (totalRating/booksCollection.length).toFixed(1);
+const averageRating = (totalRating / booksCollection.length).toFixed(1);
 
 console.log(averageRating);
 
@@ -169,3 +169,98 @@ multiply(1, 2);
 multiply(1, 2, 3);
 multiply(1, 2, 3, 4);
 
+console.log("-------------- example -------------------");
+
+const firstBook = {
+  title: "The Last Kingdom",
+  coverImage:
+    "https://images-na.ssl-images-amazon.com/images/I/51b5YG6Y1rL.jpg",
+};
+
+const { title: firstTitle, coverImage: firstCoverImage } = firstBook;
+
+console.log(firstBook);
+console.log(firstTitle, firstCoverImage);
+
+console.log("-------------- example -------------------");
+
+const booksShelf = [
+  {
+    title: "The Last Kingdom",
+    author: "Bernard Cornwell",
+    rating: 8.38,
+  },
+  {
+    title: "На березі спокійних вод",
+    author: "Роберт Шеклі",
+    rating: 8.51,
+  },
+];
+
+for (const item of booksShelf) {
+  console.log(item.title);
+  console.log(item.author);
+  console.log(item.rating);
+}
+
+console.log("-------------- example -------------------");
+
+const user = {
+  name: "Jacques Gluke",
+  tag: "jgluke",
+  stats: {
+    followers: 5603,
+    views: 4827,
+    likes: 1308,
+  },
+};
+
+const {
+  name,
+  tag,
+  stats: { followers, views = userViews, likes },
+} = user;
+
+console.log(user);
+console.log(name, views, tag, views, likes);
+
+console.log("-------------- example -------------------");
+
+const rgb = [200, 255, 100];
+
+// const [red, green, blue, alfa = 0.3] = rgb;
+
+const [red, ...colors] = rgb;
+
+console.log(rgb);
+
+// console.log(`Red: ${red}, Green: ${green}, Blue: ${blue}, Alfa ${alfa}`);
+console.log(`Red: ${red}, Colors ${colors}`);
+
+console.log("-------------- example -------------------");
+
+function doStuffWithBook(title, author) {
+  console.log(title, author);
+}
+
+// ? OR
+
+function doStuffWithBook(book) {
+  console.log(book.books, book.author);
+}
+
+// ? OR  деструктурізація в сигнатурі (підписі) функції
+
+doStuffWithBook({
+  books: ["The Last Kingdom"],
+  author: "Bernard Cornwell",
+  genres: ["hidtorical prose", "adventure"],
+  isPublic: true,
+  rating: 8.38,
+})
+
+// ? OR
+function doStuffWithBook(books) {
+  const { author, genres, isPublic, rating } = books;
+  console.log(author, genres, isPublic, rating);
+}

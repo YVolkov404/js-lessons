8ad55fa7efdfaf1316549662ce8b5c6c947e7fc9
Yurii Volkov <sir.volkov@outlook.com>
2026-02-21 11:56:07 +0200

    upd check-04,js

---
+++ js/autochecks/check-04.js
@@ -0,0 +1,224 @@
+console.log("============== Example 01 ==============");
+
+// const apartment = {
+//   imgUrl: "https://via.placeholder.com/640x480",
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+//   tags: ["premium", "promoted", "top"],
+// };
+
+// console.log(apartment);
+
+console.log("============== Example 02 ==============");
+
+// const apartment = {
+//   imgUrl: "https://via.placeholder.com/640x480",
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+//   tags: ["premium", "promoted", "top"],
+//   owner: {
+//     name: "Henry",
+//     phone: "982-126-1588",
+//     email: "henry.carter@aptmail.com",
+//   },
+// };
+
+// console.log(apartment);
+
+console.log("============== Example 03 ==============");
+
+// const apartment = {
+//   imgUrl: "https://via.placeholder.com/640x480",
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+//   tags: ["premium", "promoted", "top"],
+// };
+
+// const aptRating = apartment.rating;
+// const aptDescr = apartment.descr;
+// const aptPrice = apartment.price;
+// const aptTags = apartment.tags;
+
+// console.log(aptRating);
+// console.log(aptDescr);
+// console.log(aptPrice);
+// console.log(aptTags);
+
+console.log("============== Example 04 ==============");
+
+// const apartment = {
+//   imgUrl: "https://via.placeholder.com/640x480",
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+//   tags: ["premium", "promoted", "top"],
+//   owner: {
+//     name: "Henry",
+//     phone: "982-126-1588",
+//     email: "henry.carter@aptmail.com",
+//   },
+// };
+
+// const ownerName = apartment.owner.name;
+// const ownerPhone = apartment.owner.phone;
+// const ownerEmail = apartment.owner.email;
+// const numberOfTags = apartment.tags.length;
+// const firstTag = apartment.tags[0];
+// const lastTag = apartment.tags[2];
+
+// console.log(ownerName);
+// console.log(ownerPhone);
+// console.log(ownerEmail);
+// console.log(numberOfTags);
+// console.log(firstTag);
+// console.log(lastTag);
+
+console.log("============== Example 05 ==============");
+
+// const apartment = {
+//   imgUrl: "https://via.placeholder.com/640x480",
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+//   tags: ["premium", "promoted", "top"],
+// };
+
+// const aptRating = apartment["rating"];
+// const aptDescr = apartment["descr"];
+// const aptPrice = apartment["price"];
+// const aptTags = apartment["tags"];
+
+// console.log(aptRating);
+// console.log(aptDescr);
+// console.log(aptPrice);
+// console.log(aptTags);
+
+console.log("============== Example 06 ==============");
+
+// const apartment = {
+//   imgUrl: "https://via.placeholder.com/640x480",
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+//   tags: ["premium", "promoted", "top"],
+//   owner: {
+//     name: "Henry",
+//     phone: "982-126-1588",
+//     email: "henry.carter@aptmail.com",
+//   },
+// };
+
+// apartment.price = 5000;
+// apartment.rating = 4.7;
+// apartment.owner.name = "Henry Sibola";
+// apartment.tags.push("trusted");
+
+// console.log(apartment.price);
+// console.log(apartment.rating);
+// console.log(apartment.owner.name);
+// console.log(apartment.tags);
+
+console.log("============== Example 07 ==============");
+
+// const apartment = {
+//   imgUrl: "https://via.placeholder.com/640x480",
+//   descr: "Spacious apartment in the city center",
+//   rating: 4.7,
+//   price: 5000,
+//   tags: ["premium", "promoted", "top", "trusted"],
+//   owner: {
+//     name: "Henry Sibola",
+//     phone: "982-126-1588",
+//     email: "henry.carter@aptmail.com",
+//   },
+// };
+
+// apartment.area = 60;
+// apartment.rooms = 3;
+// apartment.location = {
+//   country: "Jamaica",
+//   city: "Kingston",
+// };
+
+console.log("============== Example 08 ==============");
+
+const name = "Repair Droid";
+const price = 2500;
+const image = "https://via.placeholder.com/640x480";
+const tags = ["on sale", "trending", "best buy"];
+
+const product = {
+  name,
+  price,
+  image,
+  tags,
+};
+
+console.log(product);
+
+console.log("============== Example 09 ==============");
+
+const emailInputName = "email";
+const passwordInputName = "password";
+
+const credentials = {
+  [emailInputName]: "henry.carter@aptmail.com",
+  [passwordInputName]: "jqueryismyjam",
+};
+
+console.log(credentials);
+
+console.log("============== Example 10 ==============");
+
+const apartment = {
+  descr: "Spacious apartment in the city center",
+  rating: 4,
+  price: 2153,
+};
+const keys = [];
+const values = [];
+// Change code below this line
+
+for (const key in apartment) {
+  keys.push(key);
+  values.push(apartment[key]);
+}
+
+console.log(keys);
+console.log(values);
+
+
+console.log("============== Example 11 ==============");
+console.log("============== Example 12 ==============");
+console.log("============== Example 13 ==============");
+console.log("============== Example 14 ==============");
+console.log("============== Example 15 ==============");
+console.log("============== Example 16 ==============");
+console.log("============== Example 17 ==============");
+console.log("============== Example 18 ==============");
+console.log("============== Example 19 ==============");
+console.log("============== Example 20 ==============");
+console.log("============== Example 21 ==============");
+console.log("============== Example 22 ==============");
+console.log("============== Example 23 ==============");
+console.log("============== Example 24 ==============");
+console.log("============== Example 25 ==============");
+console.log("============== Example 26 ==============");
+console.log("============== Example 27 ==============");
+console.log("============== Example 28 ==============");
+console.log("============== Example 29 ==============");
+console.log("============== Example 30 ==============");
+console.log("============== Example 31 ==============");
+console.log("============== Example 32 ==============");
+console.log("============== Example 33 ==============");
+console.log("============== Example 34 ==============");
+console.log("============== Example 35 ==============");
+console.log("============== Example 36 ==============");
+console.log("============== Example 37 ==============");
+console.log("============== Example 38 ==============");
+console.log("============== Example 39 ==============");
+console.log("============== Example 40 ==============");
+console.log("============== Example 41 ==============");

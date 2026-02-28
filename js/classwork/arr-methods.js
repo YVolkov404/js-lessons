160988f11741a8c012d7fef2db40d69505736f68
Yurii Volkov <sir.volkov@outlook.com>
2026-02-28 15:35:18 +0200

    upd arr-maethods

--- js/classwork/arr-methods.js
+++ js/classwork/arr-methods.js
@@ -5,9 +5,9 @@ console.log("============== Example ==============");
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 numbers.forEach(function (number, index, array) {
-    console.log(number);
-    // array[index]
-    array[4] = 20;
+  console.log(number);
+  // array[index]
+  array[4] = 20;
 });
 
 console.log(numbers);
@@ -16,9 +16,9 @@ console.log("============== Example ==============");
 
 // ? map
 
-const doubledNumbers = numbers.map(number => {
-   return number * 2
-})
+const doubledNumbers = numbers.map((number) => {
+  return number * 2;
+});
 
 console.log(doubledNumbers);
 
@@ -35,6 +35,54 @@ const students = [
 
 //? OR
 
-const studentsName = students.map(name => name.score);
+const studentsName = students.map((name) => name.score);
 
 console.log(studentsName);
+
+console.log("============== Example ==============");
+
+// ? map
+
+const players = [
+  { id: "player-1", name: "mango", timePlayed: 310, points: 54, online: false },
+  { id: "player-2", name: "polly", timePlayed: 470, points: 92, online: true },
+  { id: "player-3", name: "kiwi", timePlayed: 230, points: 48, online: true },
+  { id: "player-4", name: "ajax", timePlayed: 150, points: 71, online: false },
+  { id: "player-5", name: "chelsy", timePlayed: 80, points: 48, online: true },
+];
+
+const playerNames = players.map((player) => player.name);
+console.log("playerNames :>> ", playerNames);
+
+const playerIds = players.map((player) => player.id);
+console.log("playerIds :>> ", playerIds);
+
+const res = players.map(({ name, online }) => {
+  (name, online);
+});
+console.log("res :>> ", res);
+
+const updatedPlayers = players.map((player) => {
+  return {
+    ...player,
+    points: Math.round(player.points * 1.1),
+  };
+});
+
+console.log("updatedPlayers :>> ", updatedPlayers);
+
+console.log("============== Example ==============");
+
+console.table(players);
+
+const playerIDToUpdate = "player-3";
+
+const updatedPlayersById = players.map((player) => {
+  console.log(player.id);
+  if (playerIDToUpdate === player.id) {
+    console.log('playerIDToUpdate :>> ', playerIDToUpdate);
+    return playerIDToUpdate;
+  }
+});
+
+

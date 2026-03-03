b91882b758da360b594fa20c7487bbebb0811064
Yurii Volkov <sir.volkov@outlook.com>
2026-03-03 16:02:27 +0200

    upd lodash

--- js/classwork/lodash.js
+++ js/classwork/lodash.js
@@ -1,7 +1,8 @@
-const _ = require("lodash");
+import pkg from "lodash";
+const { isEmpty, get, union, range, sortBy, minBy } = pkg;
 
-console.log(_.isEmpty({}));
-console.log(_.isEmpty({ a: 1 }));
+console.log(isEmpty({}));
+console.log(isEmpty({ a: 1 }));
 
 console.log("============== Example ==============");
 
@@ -13,7 +14,7 @@ const user = {
   },
 };
 
-console.log(_.get(user, "location.city"));
+console.log(get(user, "location.city"));
 
 console.log(user.location.city);
 
@@ -25,11 +26,11 @@ console.log(user?.location?.city);
 
 console.log("============== Example ==============");
 
-console.log(_.union([1, 2, 3], [3, 4, 5]));
+console.log(union([1, 2, 3], [3, 4, 5]));
 
 console.log("============== Example ==============");
 
-console.log(_.range(1, 6, 2, 8, 4, 10));
+console.log(range(1, 6, 2, 8, 4, 10));
 
 console.log("============== Example ==============");
 
@@ -40,6 +41,17 @@ const cart = [
   { label: "kiwi", price: 110 },
 ];
 
-console.table(_.sortBy(cart, ['price']));
+console.table(sortBy(cart, ['price']));
 
 console.log("============== Example ==============");
+
+const players = [
+  { id: "player-1", name: "mango", timePlayed: 310, points: 54, online: false },
+  { id: "player-2", name: "polly", timePlayed: 470, points: 92, online: true },
+  { id: "player-3", name: "kiwi", timePlayed: 230, points: 48, online: true },
+  { id: "player-4", name: "ajax", timePlayed: 150, points: 71, online: false },
+  { id: "player-5", name: "chelsy", timePlayed: 80, points: 48, online: true },
+];
+
+console.log(minBy(players, player => player.points));
+

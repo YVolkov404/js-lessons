000ae41649e3b2b59c1e3b91a4f152c7867b8da4
Yurii Volkov <sir.volkov@outlook.com>
2026-03-29 17:03:02 +0300

    add events

--- js/classwork/events.js
+++ js/classwork/events.js
@@ -30,21 +30,21 @@ addListenerBtn.addEventListener("click", (event) => {
   targetBtn.addEventListener("click", onTargetBtnClick);
 });
 
-removeListenerBtn.addEventListener('click', (event) => {
-    console.log("Remove listener");
+removeListenerBtn.addEventListener("click", (event) => {
+  console.log("Remove listener");
 
-    targetBtn.removeEventListener('click', onTargetBtnClick);
-})
+  targetBtn.removeEventListener("click", onTargetBtnClick);
+});
 
-function onTargetBtnClick (event) {
-    console.log('Click on target button');
+function onTargetBtnClick(event) {
+  console.log("Click on target button");
 }
 
 console.log("----------------- Example-03 -----------------");
 
-const form = document.querySelector('.js-register-form');
+const form = document.querySelector(".js-register-form");
 
-form.addEventListener('submit', onFormSubmit)
+form.addEventListener("submit", onFormSubmit);
 
 // function onFormSubmit (event) {
 //   event.preventDefault();
@@ -61,15 +61,139 @@ form.addEventListener('submit', onFormSubmit)
 //   console.log(formData);
 // }
 
-function onFormSubmit (event) {
+function onFormSubmit(event) {
   event.preventDefault();
-  
-  const formData = new FormData (event.currentTarget);
+
+  const formData = new FormData(event.currentTarget);
 
   console.log(formData);
 
   formData.forEach((value, name) => {
     console.log(value);
     console.log(name);
-  })
+  });
+}
+
+console.log("----------------- Example-04 -----------------");
+
+const refs = {
+  input: document.querySelector(".js-input"),
+  nameLabel: document.querySelector(".js-button > span"),
+  license: document.querySelector(".js-license"),
+  btn: document.querySelector(".js-button"),
+};
+
+// refs.input.addEventListener('focus', onInputFocus);
+// refs.input.addEventListener('blur', onInputBlure);
+// refs.input.addEventListener('change', onInputChange);
+refs.input.addEventListener("input", onInputChange);
+refs.license.addEventListener("change", onLicenseChange);
+
+function onInputFocus() {
+  console.log(Focus);
+}
+
+function onInputBlure() {
+  console.log(Blur);
+}
+
+function onInputChange(event) {
+  refs.nameLabel.textContent = event.currentTarget.value;
+  if (event.currentTarget.value === "") {
+    refs.nameLabel.textContent = "Anonymous";
+  }
+}
+
+function onLicenseChange(event) {
+  event.currentTarget.checked;
+  refs.btn.disabled = !event.currentTarget.checked;
+}
+
+console.log("----------------- Example-05 -----------------");
+
+const refers = {
+  clearBtn: document.querySelector(".js-clear"),
+  output: document.querySelector(".js-output"),
+};
+
+window.addEventListener("keydown", onKeyPress);
+refers.clearBtn.addEventListener("click", onClearOutput);
+
+function onKeyPress(event) {
+  // console.log(event);
+  // console.log(event.key);
+  // console.log(event.code);
+
+  refers.output.textContent += event.key;
+}
+
+function onClearOutput() {
+  refers.output.textContent = "";
+}
+
+console.log("----------------- Example-06 -----------------");
+
+const boxRef = document.querySelector(".js-box");
+
+boxRef.addEventListener("mouseover", onMouseOver);
+boxRef.addEventListener("mouseout", onMouseOut);
+boxRef.addEventListener("mousemove", onMouseMove);
+
+function onMouseOver(event) {
+  // console.log("Mouse over");
+
+  const box = event.currentTarget;
+  box.classList.add("box-active");
+}
+
+function onMouseOut(event) {
+  // console.log("Mouse out");
+
+  const box = event.currentTarget;
+  box.classList.remove("box-active");
+}
+
+function onMouseMove(event) {
+  console.log(event);
+}
+
+console.log("----------------- Example-06 -----------------");
+
+const ref = {
+  openModalBtn: document.querySelector("[data-action='open-modal']"),
+  closeModalBtn: document.querySelector("[data-action='close-modal']"),
+  backdrop: document.querySelector(".js-backdrop"),
+};
+
+ref.openModalBtn.addEventListener("click", onOpenModal);
+ref.closeModalBtn.addEventListener("click", onCloseModal);
+ref.backdrop.addEventListener("click", onBackdropClick);
+
+function onBackdropClick(event) {
+  console.log(event.currentTarget);
+  console.log(event.target);
+
+  if (event.currentTarget === event.target) {
+    onCloseModal();
+  }
+}
+
+function onOpenModal() {
+  window.addEventListener("keydown", (event) => {
+    if (event.code === "Escape") {
+      onCloseModal();
+    }
+  });
+  document.body.classList.add("show-modal");
+  ref.closeModalBtn.addEventListener("click", onCloseModal);
+}
+
+function onCloseModal() {
+  window.removeEventListener("keydown", (event) => {
+    if (event.code === "Escape") {
+      onCloseModal();
+    }
+  });
+  document.body.classList.remove("show-modal");
+  ref.closeModalBtn.removeEventListener("click", onCloseModal);
 }
